99ee38110f1f245ed7ce244452e3cc080ee698db
Aditya Goswami <adityagoswami071@gmail.com>
2024-02-20 14:53:23 +0530

    feat: singleton pattern implemented

---
+++ Singleton Pattern/secondUse.js
@@ -0,0 +1,9 @@
+import logger from "./fancyLogger.js";
+
+
+export default function logSecondImplementation()
+{
+    logger.printLogCount()
+    logger.log('Second File')
+    logger.printLogCount()
+}
